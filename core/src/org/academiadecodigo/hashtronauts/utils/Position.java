5423c54a0023b91bc52dfc0e503a1c1d4c3ad658
César Silva <cesarsilva79@gmail.com>
2018-12-06 16:02:11 -0100

    positon class created

---
+++ core/src/org/academiadecodigo/hashtronauts/utils/Position.java
@@ -0,0 +1,20 @@
+package org.academiadecodigo.hashtronauts.utils;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class Position {
+
+    private Vector2 vector;
+
+    public Position(int x, int y) {
+        this.vector = new Vector2((float) x, (float) y);
+    }
+
+    public Vector2 getVector() {
+        return vector;
+    }
+
+    public void setVector(Vector2 vector) {
+        this.vector = vector;
+    }
+}
