8624c036e1db527695a8280c6618d721dffe2fa0
Jonathan Afonso <jonathan.ds.afonso@gmail.com>
2018-12-06 16:51:00 -0100

    added utils package with Fonts implementation

--- core/src/org/academiadecodigo/hashtronauts/menu/MainMenu.java
+++ core/src/org/academiadecodigo/hashtronauts/menu/MainMenu.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import org.academiadecodigo.hashtronauts.KillerQueen;
 
 
@@ -16,11 +17,13 @@ public class MainMenu extends ScreenAdapter {
 
     /* Screen Texts */
     private BitmapFont welcomeText;
-
-
+    private BitmapFont pressToStart;
+    private Vector2 welcomeTextSize;
+    private
 
 
     public MainMenu(KillerQueen game) {
+        this.game = game;
 
     }
 
