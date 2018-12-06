2d3c542b62b9c049be40d7e4589f235a7f990bfc
César Silva <cesarsilva79@gmail.com>
2018-12-06 17:16:40 -0100

    still under work

--- core/src/org/academiadecodigo/hashtronauts/characters/Player.java
+++ core/src/org/academiadecodigo/hashtronauts/characters/Player.java
@@ -2,28 +2,32 @@ package org.academiadecodigo.hashtronauts.characters;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 
 public class Player extends Characters {
 
-    private static  Player player = new Player();
-    private int health;
+    private static Player player = new Player();
+
     private int score;
     private Weapon weapon;
     private Texture sprite;
     private Rectangle hitbox;
+    private PlayerEvents events;
 
-    @Override
-    public int getHealth() {
-        return health;
+
+
+    private Player() {
+        super();
+        this.score = 0;
+        this.weapon = null;
+        this.spriteBatch = null;
+        this.playerRender = null;
     }
 
-    @Override
-    public void setHealth(int health) {
-        this.health = health;
-    }
 
     public static Player getInstance() {
         return player;
@@ -34,7 +38,37 @@ public class Player extends Characters {
 
     }
 
-    public void move() {
+
+    /**
+     * Renders the Player
+     */
+    @Override
+    public void render(SpriteBatch batch) {
+        batch.draw(sprite, hitbox.x, hitbox.y, hitbox.width, hitbox.height);
+
+    }
+
+    private void setEvents() {
+        Gdx.input.setInputProcessor(new InputAdapter() {
+        });
+    }
+
+
+
+
+
+
+
+    /**
+     * Updates the Player position
+     */
+
+
+
+
+
+    @Override
+    public void update() {
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
             hitbox.x -= 200 * Gdx.graphics.getDeltaTime() * 2;
         }
@@ -50,34 +84,16 @@ public class Player extends Characters {
         if (hitbox.x < 0) {
             hitbox.x = 0;
         }
-        if(hitbox.x > 736) {    // 800-64
+        if (hitbox.x > 736) {    // 800-64
             hitbox.x = 736;
         }
 
         if (hitbox.y < 0) {
             hitbox.y = 0;
         }
-        if(hitbox.y > 600) {    // 800-64
+        if (hitbox.y > 600) {    // 800-64
             hitbox.y = 600;
         }
-
-    }
-
-
-    /**
-     * Renders the Player
-     */
-    @Override
-    public void render(SpriteBatch batch) {
-        batch.draw(sprite,hitbox.x,hitbox.y,hitbox.width,hitbox.height);
-
-    }
-
-    /**
-     * Updates the Player position
-     */
-    @Override
-    public void update() {
 
     }
 
---
+++ core/src/org/academiadecodigo/hashtronauts/characters/PlayerEvents.java
@@ -0,0 +1,20 @@
+package org.academiadecodigo.hashtronauts.characters;
+
+import com.badlogic.gdx.InputAdapter;
+
+public class PlayerEvents extends InputAdapter {
+    @Override
+    public boolean keyDown(int keycode) {
+        return super.keyDown(keycode);
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return super.touchDown(screenX, screenY, pointer, button);
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return super.mouseMoved(screenX, screenY);
+    }
+}
