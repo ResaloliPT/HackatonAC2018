213cce96912f5446472712b96b071a82352e398b
Roberto "ResaloliPT" Pires <roberto_pires97@sapo.pt>
2018-12-06 15:47:54 -0100

    Created project

---
+++ desktop/src/org/academiadecodigo/hashtronauts/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.academiadecodigo.hashtronauts.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.academiadecodigo.hashtronauts.KillerQueen;
+
+public class DesktopLauncher {
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new KillerQueen(), config);
+    }
+}
