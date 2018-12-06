11c0db1911aa63683a31c5b0e29d70ab5bca32ab
Rodrigo Canto <eatsleepfarmrepeat@gmail.com>
2018-12-06 16:15:28 -0100

    fixed mistake ( added player implementation to enemy)

--- core/src/org/academiadecodigo/hashtronauts/characters/Player.java
+++ core/src/org/academiadecodigo/hashtronauts/characters/Player.java
@@ -2,5 +2,29 @@ package org.academiadecodigo.hashtronauts.characters;
 
 public class Player extends Characters {
 
+    private int score;
+    private Weapon weapon;
 
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public Weapon getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(Weapon weapon) {
+        this.weapon = weapon;
+    }
+
+    public void shoot(){
+
+    }
+    public void move(){
+
+    }
 }
