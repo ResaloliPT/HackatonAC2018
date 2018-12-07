a2317ac7358d806e08ae5918a061acfb5b1f1d9b
Rodrigo Canto <eatsleepfarmrepeat@gmail.com>
2018-12-06 23:51:02 -0100

    Cleaned code in class enemy and added wall thickness setting to game settings

--- core/src/org/academiadecodigo/hashtronauts/configs/GameSettings.java
+++ core/src/org/academiadecodigo/hashtronauts/configs/GameSettings.java
@@ -5,6 +5,7 @@ public class GameSettings {
     /* Screen Size */
     public static final int WIDTH = 600;
     public static final int HEIGHT = 800;
+    public static final int WALL_THICKNESS  = 40;
 
 
 }
--- core/src/org/academiadecodigo/hashtronauts/gameobjects/characters/Enemy.java
+++ core/src/org/academiadecodigo/hashtronauts/gameobjects/characters/Enemy.java
@@ -13,45 +13,41 @@ import org.academiadecodigo.hashtronauts.utils.Position;
 public abstract class Enemy extends Characters {
     private Texture enemyImage;
     private Rectangle hitbox;
-    private int health;
     private Position position;
     private EnemyType enemyType;
-    private boolean dead = false;
+    private int health;
 
     public Enemy(EnemyType type, Position position) {
         super(position);
         this.position = position;
         this.enemyType = type;
+        this.health = enemyType.getHealth();
         enemyImage = new Texture(type.getPath());
-        hitbox = new Rectangle();
-
     }
 
-
     private void checkCol() {
-        if (hitbox.x <= 0) {
-            hitbox.x = 0;
+
+        if (hitbox.x <= GameSettings.WALL_THICKNESS) {
+            hitbox.x = GameSettings.WALL_THICKNESS;
         }
-        if (hitbox.x >= 736) {    // 800-64
-            hitbox.x = 736;
+        if (hitbox.x >= GameSettings.WIDTH - hitbox.width - GameSettings.WALL_THICKNESS) {
+            hitbox.x = GameSettings.WIDTH - hitbox.width - GameSettings.WALL_THICKNESS;
         }
-        if (hitbox.y >= 536) {    //600-64
-            hitbox.y = 536;
+        if (hitbox.y >= GameSettings.HEIGHT - hitbox.height - GameSettings.WALL_THICKNESS) {
+            hitbox.y = GameSettings.HEIGHT;
         }
-        if (hitbox.y <= 0) {
-            hitbox.y = 0;
+        if (hitbox.y <= GameSettings.WALL_THICKNESS) {
+            hitbox.y = GameSettings.WALL_THICKNESS;
         }
     }
 
     @Override
     public void render(SpriteBatch batch) {
-        String enemyImagePath = EnemyType.SOLDIER.getPath();
-        enemyImage = new Texture(enemyImagePath);
-
-        hitbox.width = 20;
-        hitbox.height = 20;
-        hitbox.y = GameSettings.HEIGHT - hitbox.height;
-        hitbox.x = MathUtils.random(GameSettings.HEIGHT - hitbox.width);
+        int width = 20;
+        int height = 20;
+        int y = (int) (GameSettings.HEIGHT - hitbox.height);
+        int x = (int) MathUtils.random(GameSettings.WALL_THICKNESS, GameSettings.WIDTH - hitbox.width - GameSettings.WALL_THICKNESS);
+        this.hitbox = new Rectangle(x, y, width, height);
 
         batch.draw(enemyImage, hitbox.x, hitbox.y, enemyImage.getWidth(), enemyImage.getHeight());
     }
@@ -64,26 +60,14 @@ public abstract class Enemy extends Characters {
             dispose();
             return;
         }
-        int x = (int) (Math.random() + 1) * 10;
-        if (x <= 5) {
-
-            hitbox.x = 200 * (hitbox.x + Player.getInstance().getPosition().getVector().x) * 2;
-            hitbox.y = 200 * (hitbox.y + Player.getInstance().getPosition().getVector().y) * 2;
-            checkCol();
-        }
+        hitbox.x = 200 * (hitbox.x + Player.getInstance().getPosition().getVector().x);
+        hitbox.y = 200 * (hitbox.y + Player.getInstance().getPosition().getVector().y);
+        checkCol();
     }
 
     @Override
     public void dispose() {
         enemyImage.dispose();
-    }
-
-    public Rectangle getHitbox() {
-        return hitbox;
-    }
-
-    public void setHitbox(Rectangle hitbox) {
-        this.hitbox = hitbox;
     }
 
     @Override
@@ -101,14 +85,10 @@ public abstract class Enemy extends Characters {
         return health <= 0;
     }
 
-    public void setDead(boolean dead) {
-        this.dead = dead;
-    }
-
     @Override
     public void hit(int damage) {
         health -= damage;
-        if (damage > health || health <= 0){
+        if (damage > health || health <= 0) {
             health = 0;
         }
     }
